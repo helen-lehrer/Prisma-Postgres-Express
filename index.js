0c0c9e08f16e547142a9aac36bc7bf6834b10155
Helen Lehrer <hlehrer@outlook.com>
2023-01-22 22:00:01 -0800

    Connect Express, postgreSQL, and Prisma

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { PrismaClient } =  require('@prisma/client');
+const prisma = new PrismaClient();
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/job', async (req, res) => {
+  const job = await prisma.job.create({ data: req.body });
+  res.json(job);
+});
+
+app.get('/', async (req, res) => {
+  const job = await prisma.job.findMany();
+  res.json(job);
+});
+
+const port = process.env.PORT || "3000";
+
+app.listen(port, () => {
+  console.log(`Server Running at ${port} 🚀`);
+});
+
